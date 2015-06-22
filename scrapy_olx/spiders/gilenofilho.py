6c96e7b5c7f5aa48b5322d983aef6e1f3f361c63
Gileno Filho <gascf.cin@gmail.com>
2015-06-22 18:28:24 -0300

    first commit

---
+++ scrapy_olx/spiders/gilenofilho.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GilenoFilhoSpider(scrapy.Spider):
+
+    name = "gilenofilho"
+    allowed_domains = ["gilenofilho.com.br"]
+    start_urls = (
+        'http://www.gilenofilho.com.br/',
+    )
+
+    def parse(self, response):
+        self.log('Hello World: {0}'.format(response.url))
